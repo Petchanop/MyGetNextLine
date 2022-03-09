334f1ad0aa59b832893ada85d6c96cc647e36056
Petchanop <69717257+Petchanop@users.noreply.github.com>
2022-03-10 06:24:44 +0700

    "fix norm"

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,24 +6,24 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:44:18 by npiya-is          #+#    #+#             */
-/*   Updated: 2022/03/10 02:29:29 by npiya-is         ###   ########.fr       */
+/*   Updated: 2022/03/10 06:12:05 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
-        unsigned char   *psrc;
-        unsigned char   *pdst;
+	unsigned char	*psrc;
+	unsigned char	*pdst;
 
-        psrc = (unsigned char *)src;
-        pdst = (unsigned char *)dst;
-        if (!dst)
-                return (NULL);
-        while (len-- && psrc)
-                *pdst++ = *psrc++;
-        return (dst);
+	psrc = (unsigned char *)src;
+	pdst = (unsigned char *)dst;
+	if (!dst)
+		return (NULL);
+	while (len-- && psrc)
+		*pdst++ = *psrc++;
+	return (dst);
 }
 
 size_t	ft_strlen(char const *str)
@@ -39,11 +39,9 @@ size_t	ft_strlen(char const *str)
 char	*ft_strjoin(char *s1, char *s2)
 {
 	size_t	i;
-	size_t	len;
 	char	*str;
 
-	len = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc((len + 1) * sizeof(char));
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!str)
 	{
 		free(s1);
@@ -64,4 +62,33 @@ char	*ft_strjoin(char *s1, char *s2)
 	str[i + ft_strlen(s1)] = '\0';
 	free(s1);
 	return (str);
+}
+
+t_file	*ft_createptr(int fd)
+{
+	t_file	*ptr;
+
+	ptr = malloc(sizeof(t_file));
+	ptr->fd = fd;
+	ptr->stream = malloc(1 * sizeof(char));
+	ptr->stream[0] = 0;
+	ptr->start = 0;
+	return (ptr);
+}
+
+t_file	*ft_getptr(t_file *ptr, int fd)
+{
+	t_file	*new;
+	t_file	*data;
+
+	data = ptr;
+	while (data)
+	{
+		if (data->fd == fd)
+			return (data);
+		data = data->next;
+	}
+	new = ft_createptr(fd);
+	data = new;
+	return (new);
 }
