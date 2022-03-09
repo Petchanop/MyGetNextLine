505f9944b86e06edd265140bc18755fdfaa45baa
Petchanop <69717257+Petchanop@users.noreply.github.com>
2022-03-10 06:25:49 +0700

    "fix array to dynamic linked list"

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,15 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:55:59 by npiya-is          #+#    #+#             */
-/*   Updated: 2022/03/10 02:36:53 by npiya-is         ###   ########.fr       */
+/*   Updated: 2022/03/10 06:11:03 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+t_file	*ft_getptr(t_file *ptr, int fd);
+
+t_file	*ft_createptr(int fd);
 
 void	*ft_memcpy(void *dst, const void *src, size_t len);
 
@@ -27,6 +31,9 @@ char	*ft_freefile(t_file *ptr)
 	}
 	ptr->start = 0;
 	ptr->newline = 0;
+	ptr->fd = 0;
+	free(ptr);
+	ptr = NULL;
 	return (0);
 }	
 
@@ -40,41 +47,39 @@ t_file	*read_line(t_file *ptr, int fd)
 	rd = 1;
 	newline = 0;
 	i = ptr->start;
+	buff_file = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	while (rd)
 	{
-		buff_file = malloc((BUFFER_SIZE + 1) * sizeof(char));
 		rd = read(fd, buff_file, BUFFER_SIZE);
 		buff_file[rd] = 0;
 		ptr->stream = ft_strjoin(ptr->stream, buff_file);
 		while (ptr->stream[i + newline] != '\n' && ptr->stream[i + newline])
 			newline++;
 		ptr->newline = newline;
-		free(buff_file);
-		if ((ptr->stream[i + newline]) == '\n')
+		if ((ptr->stream[i + newline]) == '\n' || (rd <= 0 && ptr->stream))
+		{
+			free(buff_file);
 			return (ptr);
-		if (rd <= 0 && ptr->stream)
-			return (ptr);
+		}
 	}
+	free(buff_file);
 	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
 	t_file			*buff;
-	static t_file	ptr[10000];
+	static t_file	*ptr = NULL;
 	char			*line;
 
 	if (fd < 0)
 		return (NULL);
-	if (ptr[fd].stream == NULL && !ptr[fd].newline)
-	{
-		ptr[fd].stream = malloc(1 * sizeof(char));
-		ptr[fd].stream[0] = 0;
-		ptr[fd].start = 0;
-	}
-	buff = read_line(&ptr[fd], fd);
+	if (!ptr)
+		ptr = ft_createptr(fd);
+	buff = ft_getptr(ptr, fd);
+	buff = read_line(buff, buff->fd);
 	if (!buff->stream[buff->start])
-		return (ft_freefile(&ptr[fd]));
+		return (ft_freefile(buff));
 	if (buff->stream[buff->start + buff->newline] == '\n')
 		buff->newline++;
 	line = malloc((buff->newline + 1) * sizeof(char));
